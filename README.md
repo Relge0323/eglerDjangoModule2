5b5334b930f91885ca6575316e8e7b741bef5cfd
Relge0323 <megler@hotmail.com>
2025-09-17 12:37:17 -0700

    final
    final

--- README.md
+++ README.md
@@ -21,3 +21,6 @@ https://www.youtube.com/watch?v=b0CgA_Ap_Mc&list=PLzMcBGfZo4-kQkZp-j9PNyKq7Yw5VY
 
 (django models.py)
 https://www.youtube.com/watch?v=rI95wyHD_6k
+
+(ModelAdmin List Filters)
+https://docs.djangoproject.com/en/5.2/ref/contrib/admin/filters/#:~:text=ModelAdmin%20List%20Filters
--- main_character_syndrome/rpg/admin.py
+++ main_character_syndrome/rpg/admin.py
@@ -4,8 +4,11 @@ from .models import Character, Ability, Quest, Equipment
 # Register your models here.
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
+    #displays these field names on the admin page
     list_display = ('name', 'game', 'class_job', 'race')
+    #adds a search box for the strings in the tuple
     search_fields = ('name', 'game', 'class_job')
+    #adds a left and right column that you can choose from availble entries.
     filter_horizontal = ('abilities', 'quests')
 
 
