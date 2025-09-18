614bc9c2df4dfb2412ad692d0cd8e4734612c301
Relge0323 <megler@hotmail.com>
2025-09-18 11:20:57 -0500

    Update urls.py

--- main_character_syndrome/rpg/urls.py
+++ main_character_syndrome/rpg/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('characters/', views.character_list, name='character_list'),
+    
     # <int:pk> is a path converter that is used to help show which character was clicked in detail.
     # the pk refers to the primary key of the #th record in the table
     path('characters/<int:pk>/', views.character_detail, name='character_detail'),
